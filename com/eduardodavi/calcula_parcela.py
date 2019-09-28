751e462e02efbc4c7185413976625583e300701b
eduardo vinicius <dudustyle2@hotmail.com>
2019-09-27 21:40:57 -0300

    Finalizando os arquivos e ver os erros

--- com/eduardodavi/calcula_parcela.py
+++ com/eduardodavi/calcula_parcela.py
@@ -1,9 +1,17 @@
-def valorPagamento(valor, diasAtraso):
+'''
+Calcula a parcela e tem a função do valor do pagamento com juros ou não.
+'''
 
+
+def valor_pagamento(valor, dias_atraso):
+    '''
+    Função que verifica os valores, e vê se a multa é aplicavel ou não,
+    conforme cada caso
+    '''
     if valor < 0:
         return None
-    if diasAtraso > 0:
+    if dias_atraso > 0:
         multa = valor * 0.03
-        adicionalAtraso = valor * (diasAtraso * 0.01)
-        return valor + multa + adicionalAtraso
+        adicional_atraso = valor * (dias_atraso * 0.01)
+        return valor + multa + adicional_atraso
     return valor
