11a199fbcf6a07ab66472f6114bcc1be1188eb8a
eduardo vinicius <dudustyle2@hotmail.com>
2019-09-27 21:28:25 -0300

    Mergear Files

--- com/eduardodavi/converte_hora.py
+++ com/eduardodavi/converte_hora.py
@@ -1,13 +1,15 @@
-def converteHora(hora24, minuto24):
+def converte_hora(hora24, minuto24):
+    '''
+    Função que converte hora e devolve ela formatada
+    '''
     if (hora24 > 23) or (hora24 < 0) or (minuto24 < 0) or (minuto24 > 59):
         return None
 
-    if (hora24 < 12):
-        if (hora24 == 0):
+    if hora24 < 12:
+        if hora24 == 0:
             hora24 = 12
         return '%02d:%02d AM' % (hora24, minuto24)
 
-    if (hora24 > 12):
+    if hora24 > 12:
         hora24 -= 12
     return '%02d:%02d PM' % (hora24, minuto24)
-
