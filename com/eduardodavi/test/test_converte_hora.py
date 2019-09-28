9d847417ab36c54c45c5a09d2c4e0e0a22497efe
Leonardo Buzzo <1900861@SALAS.AULAS>
2019-09-27 21:27:40 -0300

    Arrumando Files

--- com/eduardodavi/test/test_converte_hora.py
+++ com/eduardodavi/test/test_converte_hora.py
@@ -2,38 +2,38 @@
 Converte hora
 '''
 
-from com.eduardodavi.converte_hora import converteHora
+from com.eduardodavi.converte_hora import converte_hora
 
 
 def test_hora():
     '''
     Testa a hora do metodo
     '''
-    converte_hora = converteHora(12, 30)
-    assert converte_hora == '12:30 PM', "Should be 12:30 PM"
+    converte_hora_1 = converte_hora(12, 30)
+    assert converte_hora_1 == '12:30 PM', "Should be 12:30 PM"
 
 
 def test_seis():
     '''
     Testa hora seis
     '''
-    converte_hora = converteHora(6, 30)
-    assert converte_hora == '06:30 AM', "Should be 6:30 AM"
+    converte_hora_1 = converte_hora(6, 30)
+    assert converte_hora_1 == '06:30 AM', "Should be 6:30 AM"
 
 
 def test_seis_pm():
     '''
     testa hora seis da noite
     '''
-    converte_hora = converteHora(18, 30)
-    assert converte_hora == '06:30 PM', "Should be 6:30 PM"
+    converte_hora_1 = converte_hora(18, 30)
+    assert converte_hora_1 == '06:30 PM', "Should be 6:30 PM"
 
 
 def test_meia_noite():
     '''
     testa meia noite
     '''
-    meia_noite = converteHora(0, 00)
+    meia_noite = converte_hora(0, 00)
     assert meia_noite == '12:00 AM', "Should be 12:00 am"
 
 
@@ -41,5 +41,5 @@ def test_none():
     '''
     teste none
     '''
-    none_test = converteHora(34, 30)
+    none_test = converte_hora(34, 30)
     assert none_test is None, "Should be 12:00 am"
